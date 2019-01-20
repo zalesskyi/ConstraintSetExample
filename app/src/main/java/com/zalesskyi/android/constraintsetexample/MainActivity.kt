d08980dfbabbf964b5d1e0c5bd12164f429dc957
alexeyzalesskiy <alexeyzalesskiy08@gmail.com>
2019-01-20 18:25:38 +0200

    done

--- app/src/main/java/com/zalesskyi/android/constraintsetexample/MainActivity.kt
+++ app/src/main/java/com/zalesskyi/android/constraintsetexample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.zalesskyi.android.constraintsetexample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
+import android.transition.ChangeBounds
 import android.transition.TransitionManager
+import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,14 +18,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initUi() {
-        var set = false
-        val constraint1 = ConstraintSet().apply { clone(root) }
-        val constraint2 = ConstraintSet().also { it.clone(this, R.layout.activity_main_alt) }
+        val constraintSource = ConstraintSet().also { it.clone(this, R.layout.activity_main) }
+        val constraintJava = ConstraintSet().also { it.clone(this, R.layout.activity_carousel_java) }
+        val constraintKotlin = ConstraintSet().also { it.clone(this, R.layout.activity_carousel_kotlin) }
 
-        findViewById<ImageView>(R.id.ivMain).setOnClickListener {
-            TransitionManager.beginDelayedTransition(root)
-            (constraint1.takeIf { set } ?: constraint2).applyTo(root)
-            set = !set
+        val transition = ChangeBounds().apply { interpolator = OvershootInterpolator() }
+        findViewById<ImageView>(R.id.ivJava).setOnClickListener {
+            TransitionManager.beginDelayedTransition(root, transition)
+            constraintJava.applyTo(root)
+        }
+
+        findViewById<ImageView>(R.id.ivKotlin).setOnClickListener {
+            TransitionManager.beginDelayedTransition(root, transition)
+            constraintKotlin.applyTo(root)
+        }
+
+        root.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root, transition)
+            constraintSource.applyTo(root)
         }
     }
 }
